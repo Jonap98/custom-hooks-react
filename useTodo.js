775fac746d514884594aa4304aa665d98db1e004
jona <jona_jko@hotmail.com>
2022-06-25 08:36:30 -0500

    Primer commit

---
+++ useTodo.js
@@ -0,0 +1,56 @@
+import { useEffect, useReducer } from "react"
+import { todoReducer } from "../08-useReducer/todoReducer";
+
+
+const initialState = [];
+
+const init = () => {
+    // Retornar get item, si es null, retornar arreglo vacío
+    return JSON.parse(localStorage.getItem('todos')) || [];
+}
+
+export const useTodos = () => {
+    // useReducer es una alternativa al useState
+    // El third (initializer) puede ser utilizado para un proceso pesado 
+    // const [state, dispatch] = useReducer(first, second, third)
+    const [todos, dispatch] = useReducer(todoReducer, initialState, init);
+
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos));
+
+    }, [todos])
+    
+
+    const handleNewTodo = (todo) => {
+        // Accion a mandar al reducer
+        const action = {
+            type: '[TODO] Add Todo',
+            payload: todo
+        }
+        // Mandar la acción mediante dispatch
+        dispatch(action);
+    }
+
+    const handleToggleTodo = (id) => {
+        dispatch({
+            type: '[TODO] Toggle Todo',
+            payload: id,
+        });
+    }
+
+    const handleDeleteTodo = (id) => {
+        dispatch({
+            type: '[TODO] Remove Todo',
+            payload: id
+        });
+    }
+
+    return {
+        todos,
+        todosCount: todos.length,
+        pendingTodosCount: todos.filter(todo=>!todo.done).length,
+        handleNewTodo,
+        handleToggleTodo,
+        handleDeleteTodo,
+    }
+}
